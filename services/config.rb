4032c09a18c6e67375c210649aa909e4cee77bdd
emilkip <emil@cloudcoreo.com>
2016-12-16 16:27:25 +0600

    Change alert to ec2

--- services/config.rb
+++ services/config.rb
@@ -11,22 +11,45 @@
 	## end
 	##
 	
-coreo_aws_advisor_alert "cc-emilkip-training" do
+## ELB
+# coreo_aws_advisor_alert "cc-emilkip-elb-alert" do
+# 	action :define
+# 	service :elb
+# 	display_name "ELB Object Health Check"
+# 	description "Load balancer health check"
+# 	category "Inventory"
+# 	suggested_action "None."
+# 	level "Informational"
+# 	objectives ["load_balancers"]
+# 	audit_objects ["load_balancer_description.health_check.interval"]
+# 	operators [">"]
+# 	alert_when [120]
+# end
+
+# coreo_aws_advisor_elb "cc-emilkip-advisor_elb" do
+# 	action :advise
+# 	alerts ${ELB_ALERTS}
+# 	regions ${ELB_REGIONS}
+# end
+
+
+## EC2
+coreo_aws_advisor_alert "cc-emilkip-ec2-alert" do
 	action :define
 	service :elb
-	display_name "ELB Object Health Check"
-	description "Load balancer health check"
+	display_name "EC2 check"
+	description "EC2 size and encryption check for snapshot"
 	category "Inventory"
 	suggested_action "None."
 	level "Informational"
-	objectives ["load_balancers"]
-	audit_objects ["load_balancer_description.health_check.interval"]
-	operators [">"]
-	alert_when [120]
+	objectives ["snapshots"]
+	audit_objects ["snapshot_set.volume_size", "snapshot_set.encrypted"]
+	operators [">", "=="]
+	alert_when [8, false]
 end
 
-coreo_aws_advisor_elb "cc-emilkip-training" do
+coreo_aws_advisor_ec2 "cc-emilkip-advisor_ec2" do
 	action :advise
-	alerts ${ELB_ALERTS}
-	regions ${ELB_REGIONS}
+	alerts ${EC2_ALERTS}
+	regions ${EC2_REGIONS}
 end
