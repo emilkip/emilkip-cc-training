b84a5350a1da60a385c1f6206838525bfcc694dd
emilkip <emil@cloudcoreo.com>
2016-12-16 11:47:21 +0600

    Fixes

--- services/config.rb
+++ services/config.rb
@@ -11,16 +11,16 @@
 	## end
 	##
 	
-	coreo_aws_advisor_alert "cc-emilkip-training" do
-		action :define
-		service :elb
-		display_name "ELB Object Health Check"
-		description "Load balancer health check"
-		category "Inventory"
-		suggested_action "None."
-		level "Informational"
-		objects ["load_balancers"]
-		audit_objects ["load_balancer_description.health_check.interval"]
-		operators [">"]
-		alert_when [120]
-	end
+coreo_aws_advisor_alert "cc-emilkip-training" do
+	action :define
+	service :elb
+	display_name "ELB Object Health Check"
+	description "Load balancer health check"
+	category "Inventory"
+	suggested_action "None."
+	level "Informational"
+	objectives ["load_balancers"]
+	audit_objects ["load_balancer_description.health_check.interval"]
+	operators [">"]
+	alert_when [120]
+end
